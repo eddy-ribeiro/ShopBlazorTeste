fd49fcb2025c4eff372d78eac67a32b343b35a24
Edson_Ribeiro <e.ribeiro2012@hotmail.com>
2024-01-09 18:35:00 -0300

    Commit 09/01

--- BlazorShop.web/Program.cs
+++ BlazorShop.web/Program.cs
@@ -1,11 +1,14 @@
 using BlazorShop.web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using static System.Net.WebRequestMethods;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var baseUrl = "https://localhost:7063";
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl) });
 
 await builder.Build().RunAsync();
